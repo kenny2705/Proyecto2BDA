aa671e1596af5bb00fc4ea979c10ec304a6a7083
Jorge Enriquez <95323335+TheleyendX@users.noreply.github.com>
2024-03-24 01:54:56 -0700

    En la capa negocio se le agrego la interfaz

---
+++ proyecto2Negocio/src/main/java/Main/AgenciaFiscal.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+import Persistencia.InicioApp;
+
+/**
+ *
+ * @author jorge
+ */
+public class AgenciaFiscal {
+    
+   public static void main(String[] args) {
+        InicioApp inicio = new InicioApp();
+        inicio.setVisible(true);
+        inicio.setLocationRelativeTo(null);
+    }
+    
+}
